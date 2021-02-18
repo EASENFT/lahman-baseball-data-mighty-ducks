dda10e473d7df6159d3aff261f8d2c4065e25af1
George Mills <george.r.mills@gmail.com>
2021-02-18 09:05:03 -0600

    added script file

---
+++ scripts/script_2.sql
@@ -0,0 +1,13 @@
+-- What range of years for baseball games played does the provided database cover?
+-- select max(yearid) - min(yearid), min(yearid) as first_year, max(yearid) as last_year
+-- from teams;
+
+-- Find the average number of strikeouts per game by decade since 1920. 
+-- Round the numbers you report to 2 decimal places. 
+-- Do the same for home runs per game. Do you see any trends?
+
+-- select *
+-- from battingpost
+-- -- where playerid = 'abercda01' and yearid = 1871
+-- order by yearid desc
+-- limit 50;
